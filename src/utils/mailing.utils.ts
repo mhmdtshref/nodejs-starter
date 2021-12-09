6ccbf1482b8a51a7a6a21219e687debec80d3de6
Mohamed Sharif <mshareef@kiitos-tech.com>
2021-12-09 17:38:13 +0200

    Setup SendGrid mailing utils

--- src/utils/mailing.utils.ts
+++ src/utils/mailing.utils.ts
@@ -1,11 +1,37 @@
+import axios from 'axios';
+import { EmailProps, SendGridApiBodyProps } from '@types';
 
-interface EmailProps {
-    form: string;
-    to: string;
-    templateId: string;
-    templateVariablesValues: { [key: string]: string | number };
+const emailRequestUrl = 'https://api.sendgrid.com/v3/mail/send';
+
+export const sendEmail = async ({ fromEmail, toEmail, templateId, templateVariablesValues }: EmailProps) => {
+
+    // Setup SendGrid request body:
+    const emailRequestBody: SendGridApiBodyProps = {
+        from: {
+            email: fromEmail,
+        },
+        personalizations: [
+            {
+                to: [
+                    { email: toEmail }
+                ],
+                dynamic_template_data: templateVariablesValues,
+            }
+        ],
+        template_id: templateId,
+    };
+
+    // Setup Send Grid email request headers:
+    const emailRequestHeaders = {
+        Authorization: `Bearer ${process.env.SEND_GRID_API_KEY}`,
+        'Content-Type': 'application/json',
+    };
+
+    // Send email request:
+    const emailResponse = await axios.post(emailRequestUrl, emailRequestBody, { headers: emailRequestHeaders });
+
+    // If response code success, return with success message:
+    if (emailResponse.status !== 202) {
+        throw new Error(emailResponse.data?.error?.message as string);
+    }
 }
-
-export const sendEmail = ({ form, to, templateId, templateVariablesValues } :EmailProps) => 
-    // TODO: Use function params to send email.
-     ({ form, to, templateId, templateVariablesValues })
